ef00a2a63a6d96246cd626f1d4c72a6c65ead89b
Ayush Ojha <ayushojha@Ayushs-MacBook-Air.local>
2022-09-08 12:04:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bannerad/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.bannerad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        MobileAds.initialize(this, initializationStatus -> {
+
+        });
+
+        AdView adView = findViewById(R.id.adView);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        adView.loadAd(adRequest);
+
+
+    }
+}
